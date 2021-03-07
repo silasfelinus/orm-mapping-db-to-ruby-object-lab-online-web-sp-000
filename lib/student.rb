c32283e9db54c07f807cec757ef8a9dec5a0736e
Silas Michael Knight <silasfelinus@gmail.com>
2021-03-07 23:52:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -74,7 +74,9 @@ class Student
       WHERE grade < 12
     SQL
 
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql, name).map do |row|
+     self.new_from_db(row)
+   end
   end
 
   def self.first_X_students_in_grade_10(i)
